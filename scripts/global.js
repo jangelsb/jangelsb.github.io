3b730931a6224b77b2f8e8bf565a689f2b5c9103
jangelsb <jangelsb.vcs@gmail.com>
2023-11-15 13:22:37 -0800

    * Add back imports in scss

--- scripts/global.js
+++ scripts/global.js
@@ -22,22 +22,7 @@ var MYLIBRARY = MYLIBRARY || (function(){
             _dargs = Args;
             // some other initialising
 
-            var baseCSSId = 'base_css'; 
-            if (!document.getElementById(baseCSSId))
-            {
-                var head  = document.getElementsByTagName('head')[0];
-                var link  = document.createElement('link');
-                link.id   = baseCSSId;
-                link.rel  = 'stylesheet';
-                link.type = 'text/css';
-                link.media = 'all';
-
-                link.href = '/assets/css/base.css';
-
-                head.appendChild(link);
-            }
-
-            var themeCSSId = 'custom_css'; 
+            var themeCSSId = 'theme_css'; 
             if (!document.getElementById(themeCSSId))
             {
                 var head  = document.getElementsByTagName('head')[0];
