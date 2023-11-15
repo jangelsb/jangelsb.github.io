9d7865ab064d5b93911b071a631072a7b480f2b7
jangelsb <jangelsb.vcs@gmail.com>
2023-11-15 00:31:51 -0800

    * Add scripts folder * Add global script to all pages

---
+++ _scripts/global.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+    // var server = "172.28.###.##";
+    // var url = "http://" + server + ":102/videostream.cgi?user=username&pwd=password";
+
+    document.getElementById("home_icon").href = "https://jangelsb.github.io/";
+</script>
